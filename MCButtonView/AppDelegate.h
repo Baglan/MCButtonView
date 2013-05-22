27bac67bf4967203452c4d40858f56531cf1b305
Baglan Dosmagambetov <baglan.dos@gmail.com>
2013-05-22 09:33:18 +0600

    Initial Commit

---
+++ MCButtonView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MCButtonView
+//
+//  Created by Baglan on 5/22/13.
+//  Copyright (c) 2013 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MCButtonView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCButtonView
+//
+//  Created by Baglan on 5/22/13.
+//  Copyright (c) 2013 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
